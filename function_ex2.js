805da4a81e590faebda75261897aa5ec7bc751f5
Daria <daryayo@yandex.ru>
2023-01-22 17:34:38 +0300

    HW-function-finished

--- function_ex2.js
+++ function_ex2.js
@@ -1,36 +1,39 @@
 
-let checkNum = prompt('Введите число: ', 'целое число от 2 до 1000');
-let simple;
+// Задание 2.
+
+// Написать функцию, которая принимает на входе любое число (но не больше 1 000), определяет, является ли оно простым, и выводит простое число или нет. Если введено больше 1 000, то выводится сообщение, что данные неверны. Обратите внимание на числа 0 и 1.
+
+// Здесь вам пригодятся знания из предыдущего модуля — циклы и условные операторы.
+
+
+
+let checkNum = prompt('Введите число: ', '');
 let count = 0; // для второго вариата решения
 
 function checkSimpleOfNum_1(){
-  if (typeof(+checkNum) != 'number' || isNaN(+checkNum) || +checkNum === "" || checkNum >= 1001 || +checkNum <= 1 ){
-      
-  } else if (+checkNum === 2){
-     simple = true;
-  } else if(checkNum % 1 == 0) {
-      for(let i = 2; i < checkNum; i++) { 
-           if (checkNum % i == 0) {
-           simple = false;
-           break;
-         } else {
-           simple =true;
-         }
-        }
-     }
-     
-if (simple == false){
-alert(`${checkNum} - составное число`);
-}else if(simple == true) {
-alert(`${checkNum} - простое число`);
-} else {
-alert('ошибка ввода')
-};
-
-};
+    if (checkNum<=1000 && checkNum>1){
+      if ((+checkNum) === 2){
+        return alert(`${checkNum} - простое число`)
+      }else if((+checkNum) % 1 === 0){
+          for (let i=2; i<checkNum; i++){
+            if(checkNum % i === 0){
+                alert(`${checkNum} - составное число`)
+                return
+            }
+          }
+          alert(`${checkNum} - простое число`)
+      } else {
+          alert("ошибка, введите целое число")
+      }
+    } else {
+      return alert("ошибка ввода") 
+    }
+}
 checkSimpleOfNum_1();
 
-// вариант 2 *т.к. у простого числа будет только 2 делителя можно использовать счетчик :)
+
+
+// вариант 2 *т.к. у простого числа будет только 2 делителя можно использовать счетчик
 
 function checkSimpleOfNum_2(){
   if (typeof(+checkNum) != 'number' || isNaN(+checkNum) || +checkNum === "" || checkNum >= 1001 || +checkNum <= 1 ){
@@ -56,30 +59,4 @@ if (count == 2){
 // checkSimpleOfNum_2();
 
 
-
-
-
-//3 вариант не выходит(все что не делится на 2 - простые числа)
-
-function checkSimpleOfNum_11(){
-    if (checkNum<=1000 && checkNum>1){
-      if ((+checkNum) === 2){
-        return alert(`${checkNum} - простое число`)
-      } else if((+checkNum) % 1 === 0){
-          for (let i=2; i<checkNum; i++){
-            if(checkNum % i === 0){
-             alert("составное")
-            } else {
-                alert(`${checkNum} - простое число`)
-            }
-        } 
-        } else {
-        alert("ошибка, введите целое число")
-         }
-    } else {
-      return alert("ошибка ввода") }
-}
-        
-   
-   checkSimpleOfNum_11();
    
